815eece2d3a82d1686f87cdd75aa95189d0662f8
Maik van der Gaag <maik@familie-vandergaag.nl>
2025-01-09 20:16:00 +0100

    updated to create sarif

--- test.ps1
+++ test.ps1
@@ -1,22 +1,25 @@
-$report = ""
+# Define the base directory to search for Bicep files
+$baseDir = "D:\src\github\msft-bicep"
 
-$lintResult = bicep lint "D:\src\github\msft-bicep\modules\storageaccount\storageaccount.bicep" --diagnostics-format sarif | ConvertFrom-Json
-              if ($lintResult -ne "") {
-                foreach ($run in $lintResult.runs) {
-                    $report += "### $file"
-                    foreach ($result in $run.results) {
-                        $message = $result.message.text
-                        $level = $result.level
+# Find all Bicep files in the base directory
+$bicepFiles = Get-ChildItem -Path $baseDir -Recurse -Filter *.bicep
 
-                        if($level -eq "error") {
-                            $hasErrors = $true
-                        }
-                        foreach ($location in $result.locations) {
-                            $lineNumber = $location.physicalLocation.region.startLine
-                        }
-                        $report += "$($level) - $($message) - Line: $($lineNumber)"
-                    }
-                }
-              }
+# Initialize an array to store the combined SARIF results
+$combinedSarif = @()
 
-              Write-Host $report
+# Iterate through each Bicep file and run the bicep lint command
+foreach ($file in $bicepFiles) {
+    $lintResult = bicep lint $file.FullName --diagnostics-format sarif | ConvertFrom-Json
+    if ($lintResult -ne "") {
+
+        if($combinedSarif){
+            foreach($result in $lintResult.runs[0].results){
+                $combinedSarif.runs[0].results += $result
+            }
+        }else{
+            $combinedSarif = $lintResult
+        }
+    }
+}
+
+$combinedSarif
